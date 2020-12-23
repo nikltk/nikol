a02e5d9fd91bb4745caa24d51a19446754d9f3ed
KIMJIWON08 <ling.cse.jw@gmail.com>
2020-12-23 14:53:49 +0900

    updated updater

--- nikol/update/__init__.py
+++ nikol/update/__init__.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
-# from typing import #TODO
-
 from .update import *
+
+__all__ = ['Updater']
--- nikol/update/update.py
+++ nikol/update/update.py
@@ -1,81 +1,90 @@
+import datetime
 from collections import defaultdict
 from pathlib import Path
 
 from warnings import warn
+import copy
 
 class Updater():
-	def __init__(self, comment = None, log = None):
-		self.comment = comment
-		self.log = log
-		self.tsv_path = Path('/Users/hapkim/Desktop/kr_inst/annotated-documents-split-20-min-tsv/unified_min_tsv')
-		self.patch = []
+	def __init__(self):
+		tag_list = 'mp ls ne za_pred za_ante dp_label dp_head sr_pred sr_args cr'.split(' ')
+		self.col_idx = {v:i+3 for i,v in zip(range(len(tag_list)), tag_list)}
 
-	#TODO: define attributes
-	@classmethod
-	def Config(cls, comment, log):
-		pass
+	def Config(self, tsv_path, comment, log):
+		self.tsv_path = Path(tsv_path)
+		self.comment_file = comment
+		self.log_file = log
+
 
 	def _rec_ddict(self):
 		return defaultdict(self._rec_ddict)
 
-	def load_prepatch(self, ppatch_path):
-		self.prepatch_path = Path(ppatch_path)
-		if not self.prepatch_path.suffix == '.tsv':
-			warn(f'File extension is not tsv, current: {self.prepatch_path.suffix}')		
-
-		with self.prepatch_path.open() as f:
-
+	def load_prepatch(self, ppatch_file):
+		self.patch = []
+		self.comment_list = []
 		# define pre_patch
-			self.prepatch = self._rec_ddict()
+		self.prepatch = self._rec_ddict()
 
-			for line in f:
-				one_ppatch = line.strip('\n').split('\t')
-				
-				if not len(one_ppatch) == 4:
-					raise Exception("Line not composed of 4 columns")
+		for line in ppatch_file:
+			one_ppatch = line.strip('\n').split('\t')
+			
+			if not len(one_ppatch) == 4:
+				raise Exception(f"Line not composed of 4 columns, current line: {line}, number of columns : {len(one_ppatch)}")
 
-				#check if comment
-				if not one_ppatch[-1] == '':
-					self.patch.append(one_ppatch)
+			#check if comment
+			if not one_ppatch[-1] == '':
+				self.comment_list.append('\t'.join(one_ppatch))
 
-				
-				doc_id = '-'.join(one_ppatch[0].split('-')[:2])
-				self.prepatch[doc_id][one_ppatch[0]][one_ppatch[1]] = one_ppatch[2]
-
-	def _field2idx(self):
-		tag_list = 'mp ls ne za_pred za_ante dp_label dp_head sr_pred sr_args cr'.split(' ')
-		self.col_idx = {v:i+3 for i,v in zip(range(len(tag_list)), tag_list)}
+			
+			doc_id = '-'.join(one_ppatch[0].split('-')[:2])
+			self.prepatch[doc_id][one_ppatch[0]][one_ppatch[1]] = one_ppatch[2]
 
 	def make_patch(self):
+		# self.prepa
 
-		self._field2idx()
+		self._patch_dict = self._rec_ddict()
+
 		for doc_id, gwid_items in self.prepatch.items():
 			tsv_file = self.tsv_path/f'{doc_id}.unified.min.tsv'
 
 			# print(f"loaded tsv_file: {tsv_file.stem}")
 
 			if tsv_file.exists():
-				with tsv_file.open() as f:
 
-					for line in f:
-						tsv_line= line.split('\t')
+				#TODO make shallow copy here and use it to check all elements are checked
+				
+				with tsv_file.open(encoding = 'utf8') as f: lines = f.readlines()
+				print(tsv_file.stem)
+
+				for line in lines:
+					tsv_line= line.strip('\n').split('\t')
+
+					if tsv_line:
+						print(len(tsv_line))
 
 						# if matched line exists in prepatch
 						if tsv_line[0] in self.prepatch[doc_id].keys():
 
 							for field, after in self.prepatch[doc_id][tsv_line[0]].items():
 
-								# fix morpheme unit
-								if '.' in field:
+								# fix morpheme unit in case [mp, ls, en]
+								if field.split('.')[0] in ['mp', 'ls', 'ne'] and '.' in field:
 
 									# print(f"check if field match: {field}")
 
 									field_name, sub_field = field.split('.')
 									field_idx = self.col_idx[field_name]
 
-									if not tsv_line[field_idx].split(' + ')[sub_field] == after:
+									print(tsv_line, len(tsv_line), field_idx)
+									# bug: ['NWRW1800000021-0003-00008-00001_013', 'mp.2', '닫/VV + 았/EC + 고/EC', ''] 4 3
+									# tsv_line seems to be patchline
+
+									if not tsv_line[field_idx].split(' + ')[int(sub_field)-1] == after:
 										#time, before, after, 
 										self.patch.append([tsv_line[0], field, after, ''])
+										self._patch_dict[doc_id][tsv_line[0]][field] = after
+
+									#TODO: else + delete key, check in final line
 
 								# fix word unit
 								else:
@@ -83,6 +92,8 @@ class Updater():
 									field_idx = self.col_idx[field]
 									if not tsv_line[field_idx] == after:
 										self.patch.append([tsv_line[0], field, after, ''])
+										self._patch_dict[doc_id][tsv_line[0]][field] = after
+
 
 
 			else:
@@ -90,6 +101,48 @@ class Updater():
 
 		self.patch.sort()
 
-	def write():
-		datetime.datetime.now().strftime("%y%m%d-%H%M%S")
-		pass
+	def write(self):
+		self.datenow = datetime.datetime.now().strftime("%y%m%d-%H%M%S")
+
+		if self._patch_dict:
+			for doc_id, gwid_items in self._patch_dict.items():
+				tsv_file = self.tsv_path/f'{doc_id}.unified.min.tsv'
+
+				with tsv_file.open(encoding = 'utf8') as f: lines = f.readlines()
+				for line_idx, line in enumerate(lines):
+					tsv_line= line.strip('\n').split('\t')
+					if tsv_line:
+						if tsv_line[0] in self._patch_dict[doc_id].keys():
+							for field, after in self._patch_dict[doc_id][tsv_line[0]].items(): 
+
+								if field.split('.')[0] in ['mp', 'ls', 'ne'] and '.' in field:
+									field_name, sub_idx = field.split('.')
+									field_idx = self.col_idx[field_name]
+									
+									sub_fields = tsv_line[field_idx].split(' + ')
+									sub_fields[int(sub_idx)-1] = after
+
+									tsv_line[field_idx] = ' + '.join(sub_fields)
+
+									lines[line_idx] = '\t'.join(tsv_line)
+
+
+								else:
+									field_idx = self.col_idx[field]
+
+									tsv_line[field_idx] = after
+
+									lines[line_idx] = '\t'.join(tsv_line)
+				
+				with tsv_file.open('w') as f: print('\n'.join(lines), file=f)
+
+
+		if self.comment_list:
+			print('\n'.join(self.comment_list), file = self.comment_file)
+			self.comment_file.close()
+
+		if self.patch:
+			print('\n'.join(['\t'.join([self.datenow]+i) for i in self.patch]), file = self.log_file)
+			self.log_file.close()
+
+
